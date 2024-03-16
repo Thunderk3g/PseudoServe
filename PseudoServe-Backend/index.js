69d1e3ee6b83f9d15e64b6e551b7e62ada25cc07
Thunderk3g <diwakar29buddy@gmail.com>
2024-03-16 19:08:43 +0530

    changes

--- PseudoServe-Backend/index.js
+++ PseudoServe-Backend/index.js
@@ -1,13 +1,56 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const setupRoutes = require('./route/index.route'); // Import using index.route.js
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const setupRoutes = require('./route/index.route'); // Adjusted to your file structure
+const cors = require('cors');
+const compression = require('compression');
+const errorHandler = require('./middleware/errorHandler');
+
+dotenv.config(); // This will load the environment variables from the .env file
 
 const app = express();
-const port = 3000;
+const db = require("./model/index.model"); // Adjusted to your file structure
+const Role = db.role;
 
-app.use(bodyParser.json());
-setupRoutes(app); // Setup routes using the index.route.js
+app.use(express.json({ limit: '10mb' }));
+app.use(compression());
+app.use(express.urlencoded({ limit: '10mb', extended: true, parameterLimit: 50000 }));
+app.use(cors({ optionsSuccessStatus: 200 }));
 
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => {
+        console.log('MongoDB connected');
+        initial();
+    })
+    .catch(err => console.error('MongoDB connection error:', err));
+
+setupRoutes(app); // Setup routes using the provided index.route.js
+
+app.use(errorHandler); // Use the custom error handler middleware
+
+// Define the initial function to populate roles if they don't exist
+async function initial() {
+    try {
+        const count = await Role.estimatedDocumentCount();
+        if (count === 0) {
+            await new Role({ name: "user" }).save();
+            console.log("added 'user' to roles collection");
+
+            await new Role({ name: "rider" }).save();
+            console.log("added 'rider' to roles collection");
+
+            await new Role({ name: "admin" }).save();
+            console.log("added 'admin' to roles collection");
+        }
+    } catch (err) {
+        console.error("error initializing roles:", err);
+    }
+}
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`PseudoServe listening at http://localhost:${port}`);
+    console.log(`Server is running on port ${port}`);
 });
+
+module.exports = app;
---
+++ PseudoServe-Backend/model/user.model.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+        maxlength: 200,
+    },
+    email: {
+        type: String,
+        unique: true,
+        sparse: true, // allows null but enforces uniqueness when the field is not null
+        trim: true,
+        select: false,
+    },
+    phoneNumber: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+    },
+    verified: {
+        type: Boolean,
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+    },
+    updatedAt: {
+        type: Date,
+        required: true,
+    },
+    roles: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Role"
+        }
+    ]
+});
+
+module.exports = mongoose.model('User', userSchema);
