e5a1ff6e19bf0e94d1d98cc305c73d8bb485e6b5
Thunderk3g <diwakar29buddy@gmail.com>
2024-03-17 14:03:55 +0530

    Added Login Signup

--- index.js
+++ index.js
@@ -1,36 +1,26 @@
 const express = require('express');
-<<<<<<< HEAD:PseudoServe-Backend/index.js
+const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-const setupRoutes = require('./route/index.route'); // Adjusted to your file structure
 const cors = require('cors');
 const compression = require('compression');
 const errorHandler = require('./middleware/errorHandler');
-
 dotenv.config(); // This will load the environment variables from the .env file
-
-const app = express();
-const db = require("./model/index.model"); // Adjusted to your file structure
+const { db } = require("./model/index.model"); // Adjusted to your file structure
 const Role = db.role;
-
 app.use(express.json({ limit: '10mb' }));
 app.use(compression());
 app.use(express.urlencoded({ limit: '10mb', extended: true, parameterLimit: 50000 }));
 app.use(cors({ optionsSuccessStatus: 200 }));
-=======
 const bodyParser = require('body-parser');
 const setupRoutes = require('./route/index.route'); // Import using index.route.js
-const cors = require('cors'); // Import cors package
 
-
-const app = express();
-const port = 3000;
 app.use(cors()); // Enable CORS for all routes
 app.use(bodyParser.json());
 setupRoutes(app); // Setup routes using the index.route.js
->>>>>>> 58820c0847d09a893616b3843652b766f4b2a17d:index.js
 
 // MongoDB connection
+console.log(process.env.MONGO_URL)
 mongoose.connect(process.env.MONGO_URL)
     .then(() => {
         console.log('MongoDB connected');
@@ -49,9 +39,6 @@ async function initial() {
         if (count === 0) {
             await new Role({ name: "user" }).save();
             console.log("added 'user' to roles collection");
-
-            await new Role({ name: "rider" }).save();
-            console.log("added 'rider' to roles collection");
 
             await new Role({ name: "admin" }).save();
             console.log("added 'admin' to roles collection");
--- model/index.model.js
+++ model/index.model.js
@@ -1,6 +1,15 @@
 const apiModel = require('./apiModel');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const db = {};
+
+db.mongoose = mongoose;
+
+db.role = require("./roles.model");
+db.ROLES = ["user", "rider", "admin"];
 
 module.exports = {
-    ApiModel: apiModel
-    // Export other models here
+    ApiModel: apiModel,
+    db
 };
